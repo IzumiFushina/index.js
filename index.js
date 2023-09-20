275f03a119ad7f2bc9caa6ba1a4968e019df24eb
IzumiFushina <125682908+IzumiFushina@users.noreply.github.com>
2023-09-20 13:28:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+var dataAtual = new Date();
+var dataFormatada = dataAtual.toDateString();
+  res.end('Gustavo Gabriel Cunha Manhães/nData atual: '+ dataFormatada);
+}).listen(8031);
